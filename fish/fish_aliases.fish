1e9b679d9f529ddbc40b224c2f823f4a68a6ec9b
Grayton Ward <graytonio.ward@gmail.com>
2022-06-01 23:51:13 -0400

    Removed kitty ssh alias

--- fish/fish_aliases.fish
+++ fish/fish_aliases.fish
@@ -32,9 +32,7 @@ alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 alias hw='hwinfo --short'                                   # Hardware Info
 alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
-alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'                      # List amount of -git packages
-alias wals="wal -o $HOME/config/postwal.sh -i"
-alias ssh="kitty +kitten ssh"
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
 
 # Get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
